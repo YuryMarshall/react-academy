201d86403802cb69e19c25861f536101df640e8d
Kalashnik Yury <msgkalashnik@gmail.com>
2022-12-16 13:54:49 +0300

    Added tailwind

--- lesson7/card-game/src/App.js
+++ lesson7/card-game/src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 className="bg-red-500 font-bebas text-4xl">Hello World!</h1>
     </div>
   );
 }
